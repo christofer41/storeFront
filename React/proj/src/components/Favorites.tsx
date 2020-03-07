682a43e956fba796be00d2387dd01eaced8d0ae8
petelina17 <petelina17@gmail.com>
2020-03-07 21:08:14 +0100

    #1 Finish initial components list

---
+++ React/proj/src/components/Favorites.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import {Component, CSSProperties} from 'react';
+
+export default class Favorites extends Component {
+    render() {
+        return (
+            <div style={favorites}>
+                <h1>Navbar</h1>
+            </div>
+        )
+    }
+}
+
+const favorites: CSSProperties = {
+    height: '4rem',
+    backgroundColor: '#dddddd'
+}
