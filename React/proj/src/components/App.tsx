8b960330a48a8d67a2b4ab48cc9131f4853333a1
petelina17 <petelina17@gmail.com>
2020-03-08 21:54:46 +0100

    #3 try BestBuy shop open api (fix apiKey place later)

--- React/proj/src/components/App.tsx
+++ React/proj/src/components/App.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import Layout from "./Layout";
 
+console.log('API_KEY', process.env.REACT_APP_WEATHER_API_KEY)
 
 export default class App extends React.Component {
     render() {
