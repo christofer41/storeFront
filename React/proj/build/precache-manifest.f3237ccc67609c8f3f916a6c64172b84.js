e5b87e9e952382092a08a6e19a0be8d892d254b2
petelina17 <petelina17@gmail.com>
2020-03-17 20:17:22 +0100

    build

---
+++ React/proj/build/precache-manifest.f3237ccc67609c8f3f916a6c64172b84.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "997c2adaefada3aaf75c491b95104e6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1115cc770a15426aa31",
+    "url": "/static/js/2.4d6ff226.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.4d6ff226.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82e6388ab8efa42b16ff",
+    "url": "/static/js/main.2bfcb1e5.chunk.js"
+  },
+  {
+    "revision": "d2e3d287abbff0085f2e",
+    "url": "/static/js/runtime-main.9d651b5c.js"
+  }
+]);
